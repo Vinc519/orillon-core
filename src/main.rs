6b082cfad77be637a1e99bab798b75a9f753bb0c
PINGAUX Aymeric <pingaux@et.esiea.fr>
2026-09-11 21:55:26 +0200

    Create structures to make the test passed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+#[derive(Debug, PartialEq)]
+pub enum Action {
+    Allow,
+    Deny,
+}
+
+pub struct Packet {
+
+}
+
+impl Packet {
+    pub fn tcp(src: &str, dst: &str, port: u16) -> Self {
+        Self {}
+    }
+}
+
+pub struct Rule {
+
+}
+
+pub struct RuleEngine {
+    rules: Vec<Rule>,
+}
+
+impl RuleEngine {
+    pub fn new(rules: Vec<Rule>) -> Self {
+        Self { rules }
+    }
+
+    pub fn decide(&self, packet: &Packet) -> Action {
+        Action::Deny
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
@@ -7,9 +41,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_rule() {
+    fn no_rules_means_packet_is_denied_by_default() {
         let engine = RuleEngine::new(vec![]);
         let packet = Packet::tcp("10.0.0.1", "10.0.0.2", 443);
-        assert_eq!(engine.decide(&packet), Action::Allow);
+        assert_eq!(engine.decide(&packet), Action::Deny);
     }
 }
