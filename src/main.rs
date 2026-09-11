b961131554c6f26d31eca375ce97d56d2e278f11
PINGAUX Aymeric <pingaux@et.esiea.fr>
2026-09-11 21:17:44 +0200

    Add a test fo the engine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rule() {
+        let engine = RuleEngine::new(vec![]);
+        let packet = Packet::tcp("10.0.0.1", "10.0.0.2", 443);
+        assert_eq!(engine.decide(&packet), Action::Allow);
+    }
+}
