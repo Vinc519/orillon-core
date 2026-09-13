02a50f02b434976ed7b495ee7aefbdd69b25f6aa
PINGAUX Aymeric <pingaux@et.esiea.fr>
2026-09-13 12:40:59 +0200

    Adding the support of source port for a rule

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,16 @@ pub struct Packet {
     protocol: Protocol,
     src: String,
     dst: String,
-    port: u16,
+    sport: u16,
 }
 
 impl Packet {
-    pub fn tcp(src: &str, dst: &str, port: u16) -> Self {
+    pub fn tcp(src: &str, dst: &str, sport: u16) -> Self {
         Self {
             protocol: Protocol::Tcp,
             src: src.to_string(),
             dst: dst.to_string(),
-            port,
+            sport: sport,
         }
     }
 }
@@ -32,6 +32,7 @@ pub struct Rule {
     protocol: Option<Protocol>,
     src: Option<String>,
     dst: Option<String>,
+    sport: Option<u16>,
     action: Option<Action>,
 }
 
@@ -41,6 +42,7 @@ impl Rule {
             protocol: None,
             src: None,
             dst: None,
+            sport: None,
             action: None,
         }
     }
@@ -57,6 +59,11 @@ impl Rule {
 
     pub fn dst(mut self, dst: &str) -> Self {
         self.dst = Some(dst.to_string());
+        self
+    }
+
+    pub fn sport(mut self, sport: u16) -> Self {
+        self.sport = Some(sport);
         self
     }
 
@@ -77,9 +84,10 @@ impl RuleEngine {
 
     pub fn decide(&self, packet: &Packet) -> Action {
         for rule in &self.rules {
-            if rule.src.as_deref().is_none_or(|src| src == packet.src)
-            && rule.protocol.is_none_or(|protocol| protocol == packet.protocol)
-            && rule.dst.as_deref().is_none_or(|dst| dst == packet.dst) {
+            if rule.protocol.is_none_or(|protocol| protocol == packet.protocol)
+            && rule.src.as_deref().is_none_or(|src| src == packet.src)
+            && rule.dst.as_deref().is_none_or(|dst| dst == packet.dst)
+            && rule.sport.is_none_or(|sport| sport == packet.sport) {
                 return rule.action.unwrap_or(Action::Deny);
             }
         }
@@ -134,7 +142,25 @@ mod tests {
     fn destination_non_matching() {
         let rule = Rule::new().protocol(Protocol::Tcp).src("10.0.0.1").dst("10.0.0.4").action(Action::Allow);
         let engine = RuleEngine::new(vec![rule]);
-        let packet = Packet::tcp("10.0.0.4", "10.0.0.1", 443);
+        let packet = Packet::tcp("10.0.0.4", "10.0.0.1", 53);
+
+        assert_eq!(engine.decide(&packet), Action::Deny);
+    }
+
+    #[test]
+    fn sport_matching() {
+        let rule = Rule::new().protocol(Protocol::Tcp).src("10.0.0.15").dst("10.0.0.26").sport(22).action(Action::Allow);
+        let engine = RuleEngine::new(vec![rule]);
+        let packet = Packet::tcp("10.0.0.15", "10.0.0.26", 22);
+
+        assert_eq!(engine.decide(&packet), Action::Allow);
+    }
+
+    #[test]
+    fn sport_non_matching() {
+        let rule = Rule::new().protocol(Protocol::Tcp).src("10.0.0.55").dst("10.0.0.43").sport(143).action(Action::Allow);
+        let engine = RuleEngine::new(vec![rule]);
+        let packet = Packet::tcp("10.0.0.55", "10.0.0.43", 443);
 
         assert_eq!(engine.decide(&packet), Action::Deny);
     }
